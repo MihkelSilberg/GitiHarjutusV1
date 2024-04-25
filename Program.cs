365f1ea969332509202c72710cc04d64a909e7a4
opilane <opilane@HP-CZC0359NN7>
2024-04-25 08:52:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GitiHarjutusV1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            
+        }
+    }
+}
